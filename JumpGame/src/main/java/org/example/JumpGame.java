d5493fdec69f3f2b153e437959fb7672d8f8685a
Till Rohrmann <trohrmann@apache.org>
2020-11-20 09:57:38 +0100

    Speed up jump game 2

--- JumpGame/src/main/java/org/example/JumpGame.java
+++ JumpGame/src/main/java/org/example/JumpGame.java
@@ -1,7 +1,10 @@
 package org.example;
 
 import java.util.ArrayDeque;
+import java.util.HashSet;
 import java.util.Queue;
+import java.util.Set;
+import java.util.function.IntFunction;
 
 /**
  * https://leetcode.com/problems/jump-game-ii/
@@ -24,7 +27,7 @@ public class JumpGame {
 
             final int reach = nums[currentState.getIndex()];
 
-            for (int i = Math.min(reach, targetIndex - currentState.getIndex()); i >= 1 ; i--) {
+            for (int i = Math.min(reach, targetIndex - currentState.getIndex()); i >= 1; i--) {
                 final int nextState = currentState.getIndex() + i;
 
                 if (!visited[nextState]) {
@@ -35,6 +38,31 @@ public class JumpGame {
         }
 
         throw new IllegalArgumentException("Could not reach the end.");
+    }
+
+    public static int jumpFast(int[] nums) {
+        int steps = 0;
+        int max = 0;
+        int current = 0;
+        int newMax = 0;
+
+        while (true) {
+            if (max >= nums.length - 1) {
+                return steps;
+            }
+            steps++;
+
+            while (current <= max) {
+
+                if (current + nums[current] > newMax) {
+                    newMax = current + nums[current];
+                }
+
+                current++;
+            }
+
+            max = newMax;
+        }
     }
 
     public static final class State {
--- JumpGame/src/test/java/org/example/JumpGameTest.java
+++ JumpGame/src/test/java/org/example/JumpGameTest.java
@@ -9,17 +9,27 @@ public class JumpGameTest {
 
     @Test
     public void testJumpSolutionCase1() {
-        assertThat(JumpGame.jump(new int[]{2,3,1,1,4}), is(2));
+        assertThat(JumpGame.jumpFast(new int[]{2,3,1,1,4}), is(2));
     }
 
     @Test
     public void testJumpSolutionCase2() {
-        assertThat(JumpGame.jump(new int[]{2,3,0,1,4}), is(2));
+        assertThat(JumpGame.jumpFast(new int[]{2,3,0,1,4}), is(2));
     }
 
     @Test
     public void testJumpSolutionCase3() {
-        assertThat(JumpGame.jump(new int[]{8,2,4,4,4,9,5,2,5,8,8,0,8,6,9,1,1,6,3,5,1,2,6,6,0,4,8,6,0,3,2,8,7,6,5,1,7,0,3,4,8,3,5,9,0,4,0,1,0,5,9,2,0,7,0,2,1,0,8,2,5,1,2,3,9,7,4,7,0,0,1,8,5,6,7,5,1,9,9,3,5,0,7,5}), is(13));
+        assertThat(JumpGame.jumpFast(new int[]{8,2,4,4,4,9,5,2,5,8,8,0,8,6,9,1,1,6,3,5,1,2,6,6,0,4,8,6,0,3,2,8,7,6,5,1,7,0,3,4,8,3,5,9,0,4,0,1,0,5,9,2,0,7,0,2,1,0,8,2,5,1,2,3,9,7,4,7,0,0,1,8,5,6,7,5,1,9,9,3,5,0,7,5}), is(13));
+    }
+
+    @Test
+    public void testJumpSolutionCase4() {
+        assertThat(JumpGame.jumpFast(new int[]{0}), is(0));
+    }
+
+    @Test
+    public void testJumpSolutionCase5() {
+        assertThat(JumpGame.jumpFast(new int[]{1, 2}), is(1));
     }
 
 }
